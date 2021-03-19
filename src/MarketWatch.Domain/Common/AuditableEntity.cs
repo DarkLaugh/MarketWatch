ffa6d87e27e30cc8e8f5f0877e60d738c844fac5
Gencho Draganov <gencho.draganov@scalefocus.com>
2021-03-19 21:58:23 +0200

    Created project skeleton and added Domain Models

---
+++ src/MarketWatch.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarketWatch.Domain.Common
+{
+    public class AuditableEntity<T> : BaseEntity<T>
+    {
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
---
+++ src/MarketWatch.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace MarketWatch.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public List<Comment> Comments { get; set; }
+    }
+}
