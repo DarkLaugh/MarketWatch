edc572ed00be202d376d00b310ba03503f4474f2
Gencho Draganov <gencho.draganov@scalefocus.com>
2021-03-25 21:18:56 +0200

    Registered AutoMapper in Starup via Dependency Inejction

--- src/MarketWatch.WebUI/Startup.cs
+++ src/MarketWatch.WebUI/Startup.cs
@@ -1,15 +1,9 @@
+using MarketWatch.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MarketWatch.WebUI
 {
@@ -25,6 +19,7 @@ namespace MarketWatch.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
             services.AddControllers();
         }
 
