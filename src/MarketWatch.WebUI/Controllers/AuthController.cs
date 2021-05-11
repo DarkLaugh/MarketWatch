69b6e1843ca8da58e0b95e09bf3b1c12e4f3d84a
Gencho Draganov <gencho.draganov@scalefocus.com>
2021-05-11 11:43:31 +0300

    Added Controllers and CORS policy

---
+++ src/MarketWatch.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using MarketWatch.Application.DTOs.Requests;
+using MarketWatch.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MarketWatch.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestModel request)
+        {
+            string token = await authService.Register(request);
+
+            return Ok(new { Token = token });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestModel request)
+        {
+            string token = await authService.Login(request);
+
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ src/MarketWatch.WebUI/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using MarketWatch.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace MarketWatch.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            this._stockService = stockService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStocks()
+        {
+            var result = await _stockService.GetAllStocksAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{stockId}")]
+        public async Task<IActionResult> GetStockById(Guid stockId)
+        {
+            var result = await _stockService.GetStockByIdAsync(stockId);
+
+            return Ok(result);
+        }
+    }
+}
